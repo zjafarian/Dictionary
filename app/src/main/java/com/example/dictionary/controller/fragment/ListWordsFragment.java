a7cd4f64e8cb84f288d7f002cb5c842e5294b0d8
zjafarian <zjafarian88@gmail.com>
2020-10-04 22:02:11 +0330

    complete logic ListWordsFragment

--- app/src/main/java/com/example/dictionary/controller/fragment/ListWordsFragment.java
+++ app/src/main/java/com/example/dictionary/controller/fragment/ListWordsFragment.java
@@ -2,18 +2,30 @@ package com.example.dictionary.controller.fragment;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 import com.example.dictionary.R;
+import com.example.dictionary.model.Word;
+import com.example.dictionary.repository.IRepository;
+import com.example.dictionary.repository.WordsDBRepository;
+import com.google.android.material.textview.MaterialTextView;
+
+import java.util.List;
 
 public class ListWordsFragment extends Fragment {
     private RecyclerView mRecyclerView;
-
+    private IRepository mWordRepository;
+    private List<Word> mWords;
+    private boolean mCheck;
+    private WordAdapter mWordAdapter;
 
 
     public ListWordsFragment() {
@@ -33,6 +45,10 @@ public class ListWordsFragment extends Fragment {
         super.onCreate(savedInstanceState);
         setHasOptionsMenu(true);
 
+        mWordRepository = WordsDBRepository.getInstance(getActivity());
+        mWords = mWordRepository.getWords();
+
+
     }
 
     @Override
@@ -41,10 +57,111 @@ public class ListWordsFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_list_words, container, false);
         findViews(view);
+        initViews();
         return view;
     }
 
     private void findViews(View view) {
         mRecyclerView = view.findViewById(R.id.recycle_view_words);
     }
+
+    private void initViews() {
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        mWordAdapter =new WordAdapter(mWords);
+        mRecyclerView.setAdapter(mWordAdapter);
+    }
+
+    private class WordHolder extends RecyclerView.ViewHolder {
+
+        private MaterialTextView mTextWord;
+        private MaterialTextView mTextMeaning;
+        private ImageButton mImgBtnEdit;
+        private ImageButton mImgBtnRemove;
+        private Word mWord;
+
+        public WordHolder(@NonNull View itemView) {
+            super(itemView);
+
+            findViews(itemView);
+
+            setListener();
+
+
+        }
+
+        private void setListener() {
+            mImgBtnEdit.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                }
+            });
+
+
+            mImgBtnRemove.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                }
+            });
+        }
+
+        private void findViews(@NonNull View itemView) {
+            mTextWord = itemView.findViewById(R.id.text_view_word);
+            mTextMeaning = itemView.findViewById(R.id.text_view_meaning);
+            mImgBtnEdit = itemView.findViewById(R.id.img_btn_edit);
+            mImgBtnRemove = itemView.findViewById(R.id.img_btn_remove);
+        }
+
+        private void bindWord(Word word) {
+            mWord = word;
+            if (!mCheck) {
+                mTextWord.setText(word.getWord());
+                mTextMeaning.setText(word.getMeaning());
+            } else {
+                mTextWord.setText(word.getMeaning());
+                mTextMeaning.setText(word.getWord());
+            }
+        }
+    }
+
+
+    private class WordAdapter extends RecyclerView.Adapter<WordHolder> {
+        private List<Word> mWordList;
+
+        public WordAdapter(List<Word> words) {
+            mWordList = words;
+        }
+
+
+        public List<Word> getWordList() {
+            return mWordList;
+        }
+
+        public void setWordList(List<Word> wordList) {
+            mWordList = wordList;
+        }
+
+        @NonNull
+        @Override
+        public WordHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            LayoutInflater layoutInflater = LayoutInflater.from(getActivity());
+            View view = layoutInflater.inflate(R.layout.word_row_list, parent, false);
+            WordHolder wordHolder = new WordHolder(view);
+            return wordHolder;
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull WordHolder holder, int position) {
+            Word word = mWordList.get(position);
+            holder.bindWord(word);
+        }
+
+        @Override
+        public int getItemCount() {
+            return mWordList.size();
+        }
+    }
+
+
 }
