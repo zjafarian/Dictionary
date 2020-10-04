218551302e9c7986182e1ea31745a9c0176845aa
zjafarian <zjafarian88@gmail.com>
2020-10-04 14:08:41 +0330

    add add,search,remove,edit icon in drawable create list_menu.xml

--- app/src/main/java/com/example/dictionary/controller/fragment/ListWordsFragment.java
+++ app/src/main/java/com/example/dictionary/controller/fragment/ListWordsFragment.java
@@ -10,11 +10,6 @@ import android.view.ViewGroup;
 
 import com.example.dictionary.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ListWordsFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class ListWordsFragment extends Fragment {
 
 
