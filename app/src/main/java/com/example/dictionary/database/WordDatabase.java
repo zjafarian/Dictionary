253adc3e74b3336725e4d8a3bffe045a98dfa3d8
zjafarian <zjafarian88@gmail.com>
2020-10-04 21:30:34 +0330

    complete logic for WordDatabase: extend RoomDatabase complete logic for WordsDBRepository

--- app/src/main/java/com/example/dictionary/database/WordDatabase.java
+++ app/src/main/java/com/example/dictionary/database/WordDatabase.java
@@ -1,11 +1,12 @@
 package com.example.dictionary.database;
 
 import androidx.room.Database;
+import androidx.room.RoomDatabase;
 
 import com.example.dictionary.model.Word;
 
 @Database(entities = Word.class, version = 1)
-public abstract class WordDatabase {
+public abstract class WordDatabase extends RoomDatabase {
 
     public abstract WordDAO getWordTable();
 
--- app/src/main/java/com/example/dictionary/repository/WordsDBRepository.java
+++ app/src/main/java/com/example/dictionary/repository/WordsDBRepository.java
@@ -2,6 +2,10 @@ package com.example.dictionary.repository;
 
 import android.content.Context;
 
+import androidx.room.Room;
+
+import com.example.dictionary.database.WordDAO;
+import com.example.dictionary.database.WordDatabase;
 import com.example.dictionary.model.Word;
 
 import java.util.List;
@@ -9,12 +13,18 @@ import java.util.List;
 public class WordsDBRepository implements IRepository {
     private static WordsDBRepository sInstance;
     private Context mContext;
+    private WordDAO mWordDAO;
 
     private WordsDBRepository(Context context) {
         mContext = context.getApplicationContext();
 
+        WordDatabase wordDatabase = Room.databaseBuilder(mContext,
+                WordDatabase.class,
+                "word.db")
+                .allowMainThreadQueries()
+                .build();
 
-
+        mWordDAO = wordDatabase.getWordTable();
     }
 
     public static WordsDBRepository getInstance(Context context) {
@@ -26,26 +36,28 @@ public class WordsDBRepository implements IRepository {
 
     @Override
     public List<Word> getWords() {
-        return null;
+        return mWordDAO.getWords();
     }
 
     @Override
     public Word getWord(long id) {
-        return null;
+        return mWordDAO.getWord(id);
     }
 
     @Override
     public void insertWord(Word word) {
+        mWordDAO.insertWord(word);
 
     }
 
     @Override
     public void updateWord(Word word) {
+        mWordDAO.updateWord(word);
 
     }
 
     @Override
     public void deleteWord(Word word) {
-
+        mWordDAO.deleteWord(word);
     }
 }
